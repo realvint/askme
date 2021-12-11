949da87053e502b282706eb522a5998ff470da21
realvint <victor.malukhin@gmail.com>
2021-12-11 21:55:04 +0300

    add downcase username callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,8 @@ class User < ApplicationRecord
 
   attr_accessor :password
 
+  before_validation :downcase_username
   before_save :encrypt_password
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(password, password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
 
   def self.hash_to_string(password_hash)
     password_hash.unpack1('H*')
@@ -36,6 +28,21 @@ class User < ApplicationRecord
     if user.present? && user.password_hash == User
        .hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
       user
+    end
+  end
+
+  private
+
+  def downcase_username
+    username&.downcase!
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(password, password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
     end
   end
 end
