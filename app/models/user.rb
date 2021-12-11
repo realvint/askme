d6262a74171d2c2165757e2f5bd0314875ad2401
realvint <victor.malukhin@gmail.com>
2021-12-11 20:35:06 +0300

    add authenticate method for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,39 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  ITERATIONS = 20_000
+  DIGEST = OpenSSL::Digest.new('SHA256')
+
   has_many :questions
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true
+
+  attr_accessor :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(password, password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack1('H*')
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    if user.present? && user.password_hash == User
+       .hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    end
+  end
 end
