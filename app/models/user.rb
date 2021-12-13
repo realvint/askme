42f2bc4bb3faff5bfeb630da9031ca57a4e7caf6
realvint <victor.malukhin@gmail.com>
2021-12-14 00:12:12 +0300

    update authenticate method for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   before_save :encrypt_password
 
   def self.authenticate(email, password)
-    user = find_by(email: email)
+    user = find_by(email: email.downcase)
 
     if user.present? && user.password_hash == User
        .hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
