1c0d519f0c6105aa97ca0283bc1a9375ef9cfc5c
realvint <victor.malukhin@gmail.com>
2021-12-21 20:58:35 +0300

    add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit show update]
+  before_action :authorize_user, except: %i[index new create show]
 
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
     @user = User.new
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
     @user = User.new(user_params)
 
     if @user.save
@@ -40,6 +45,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
 
   def set_user
     @user ||= User.find params[:id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'users#index'
+
   resources :users, except: [:destroy]
+  resources :sessions, only: %i[new create destroy]
   resources :questions
+
+  get 'sign_up' => 'users#new'
+  get 'log_in' => 'sessions#new'
+  get 'log_out' => 'sessions#destroy'
 end
