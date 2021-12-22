cf7642456e63f922c1c7760347e5f4f91b6d5f4d
realvint <victor.malukhin@gmail.com>
2021-12-22 14:50:43 +0300

    update fontawesome config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import Rails from '@rails/ujs'
 import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 import './utilities/form_hider'
-import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
